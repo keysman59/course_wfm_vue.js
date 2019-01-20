72b46973a15e7ef8cda4d50862625675f2457fb9
keysman59 <keysman.dima@yandex.ru>
2019-01-20 18:46:27 +0300

    first commit_lesson_31

---
+++ lesson 31/app-name/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Parent: {{ carName }}</h1>
+    <app-car 
+      v-bind:carName="carName"
+      v-bind:carYear="carYear"
+    ></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      carName: 'Ford from parent',
+      carYear: '2018'
+    }
+  },
+  components: {
+    appCar: Car
+  }
+}
+</script>
