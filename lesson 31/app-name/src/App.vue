bbf2496b420913e88ecb2ab491b54dbeaa4a3e1f
keysman59 <keysman.dima@yandex.ru>
2019-01-20 22:19:22 +0300

    lesson 32

--- lesson 31/app-name/src/App.vue
+++ lesson 31/app-name/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <h1>Parent: {{ carName }}</h1>
-    <app-car 
-      v-bind:carName="carName"
-      v-bind:carYear="carYear"
-    ></app-car>
+    <h1>{{ carName }}</h1>
+
+    <app-car :carName="carName"
+             :carYear="carYear"
+              ></app-car>
   </div>
 </template>
 
@@ -12,11 +12,10 @@
 import Car from './Car.vue'
 
 export default {
-  name: 'app',
   data() {
     return {
       carName: 'Ford from parent',
-      carYear: '2018'
+      carYear: 2018
     }
   },
   components: {
--- lesson 31/app-name/src/Car.vue
+++ lesson 31/app-name/src/Car.vue
@@ -10,7 +10,7 @@
 <script>
 
 export default {
-    props: ['carName','carYear']
+    props: ['carName','carYear'],
 }
 
 </script>
